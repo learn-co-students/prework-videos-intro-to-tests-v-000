5d25b27d3c665d93fda418efad906232315e46b6
Brian Arango <brianvictus@gmail.com>
2019-11-25 23:12:22 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,17 +1,17 @@
 require_relative './conversions.rb'
 
-puts "Given 0, it should return 0.0"
-answer = ounces_to_grams(0)
-if answer == 0.0
+puts "ounces to grams, when given 0, returns 0.0"
+answers = ounces_to_grams(0)
+if answers == 0
   puts "Passed!"
-else
+else 
   puts "Test failed: got #{answer} instead"
 end
 
-puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
-if answer == 28.3495
+puts "ounces to grams, when given 1, returns 28.3495"
+answers = ounces_to_grams(1)
+if answers == 28.3495
   puts "Passed!"
-else
+else 
   puts "Test failed: got #{answer} instead"
 end
