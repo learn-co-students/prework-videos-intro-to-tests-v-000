ad1d6319f806cfcf75ad812b6b71f3f730227c28
alice T <alicewtien@gmail.com>
2019-11-26 16:59:58 +0000

    Done.

--- conversions.rb
+++ conversions.rb
@@ -1,4 +1,9 @@
-GRAMS_PER_OUNCE = 28.3495
-def ounces_to_grams(ounces)
-  ounces.to_f * GRAMS_PER_OUNCE
+#GRAMS_PER_OUNCE = 28.3495
+#def ounces_to_grams(ounces)
+  #ounces.to_f * GRAMS_PER_OUNCE
+#end
+
+ML_PER_CUP = 236.588
+def cups_to_mls(cups)
+  cups.to_f * ML_PER_CUP
 end
