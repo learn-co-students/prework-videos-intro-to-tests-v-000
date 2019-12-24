873340124c22003112db53cd02a96a771364dc5c
Nate Dietz <nsdietz@gmail.com>
2019-12-24 07:26:07 -0500

    Done.

--- conversions.rb
+++ conversions.rb
@@ -1,4 +1,9 @@
 GRAMS_PER_OUNCE = 28.3495
+OUNCES_PER_POUND = 16.0
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
+end
+
+def pounds_to_ounces(pounds)
+  pounds.to_f / OUNCES_PER_POUND
 end
