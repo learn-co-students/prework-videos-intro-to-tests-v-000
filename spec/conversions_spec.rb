0c052c7dac76efc8df3b36c9908213bf9cba13bf
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 17:21:20 +0000

    Done.

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../conversions.rb'
 
+puts "Tests are running..."
+
 describe "conversions" do
-  describe '#ounces_to_grams' do
-    it 'given zero, returns 0.0' do
+  describe "#ounces_to_grams" do
+    it 'given 0, returns 0.0' do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
     end
