f0e88bcde19312a3e676033e40c955ed2edbb229
Jillian Weimer <jilliannweimer@gmail.com>
2020-04-10 15:15:47 +0000

    Done.

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,10 +1,9 @@
 require_relative '../conversions.rb'
 
-describe "conversions" do
-  describe '#ounces_to_grams' do
-    it 'given zero, returns 0.0' do
+desbribe "conversions" do
+  describe "#ounces_to_grams" do
+    it "given 0, returns 0.0"
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
-    end
   end
 end
